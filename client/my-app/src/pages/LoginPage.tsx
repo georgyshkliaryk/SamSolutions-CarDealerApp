c19da1cd033568a959c68614e7d93f19fa060b00
georgyshkliaryk <shkliargosh23@gmail.com>
2020-03-05 01:58:24 +0100

    login

--- client/my-app/src/pages/LoginPage.tsx
+++ client/my-app/src/pages/LoginPage.tsx
@@ -1,20 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Login from '../components/Login/Login';
 
 const LoginPage = () => {
   return (
-    <div>
-      <h2>Login/Register</h2>
-      <input type="text" />
-      <br />
-      <input type="text" />
-      <br />
-      <Link to="/404">
-        <button>Login</button>
-        <br />
-      </Link>
-      <Link to="/">Sign in as a guest</Link>
-    </div>
+    <>
+      <Login />
+    </>
   );
 };
 
