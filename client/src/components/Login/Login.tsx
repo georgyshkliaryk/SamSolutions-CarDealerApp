860d7da60fa6b46cce241c599ae674b9a9c5cbab
georgyshkliaryk <shkliargosh23@gmail.com>
2020-03-14 02:02:37 +0100

    login validation

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -3,16 +3,38 @@ import { Link } from "react-router-dom";
 import "./Login.scss";
 import SearchButton from "../buttons/SearchButton/SearchButton";
 
+interface ILogin {
+  loginError: string;
+  passwordError: string;
+  confirmPasswordError: string;
+  loginValue: string;
+  passwordValue: string;
+}
+
 class Login extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
   }
-  state = {
-      loginError: "",
-      passwordError: "",
-      loginValue: "",
-      passwordValue: ""
+  state: ILogin = {
+    loginError: "",
+    passwordError: "",
+    confirmPasswordError: "",
+    loginValue: "",
+    passwordValue: ""
   };
+
+  handleChangeValue(event) {
+    this.setState({
+      loginValue: event.target.value
+    });
+  }
+
+  handleChangePassword(event) {
+    this.setState({
+      passwordValue: event.target.value
+    });
+  }
+
   render() {
     return (
       <div className="login">
@@ -24,39 +46,78 @@ class Login extends React.Component<any, any> {
               className="login__container-form"
               onSubmit={event => {
                 event.preventDefault(); // Prevents page reloading
-                this.setState({
-                  loginValue: this.props.value,
-                  passwordValue: "kekw"
-                });
-                alert(this.state.loginValue.toString());
+
+                const loginRegExp = new RegExp("^[a-zA-Z][a-zA-Z0-9-_\.]{0,20}$");
+                const passwordRegExp = new RegExp("^[a-zA-Z][a-zA-Z0-9-_\.]{0,20}$");
+
+                if (this.state.loginValue == "") {      //login value check
+                  this.setState({
+                    loginError: "enter your login!" 
+                  });
+                } else {
+                  if (loginRegExp.test(this.state.loginValue) == false) { 
+                    this.setState({
+                      loginError: "only A..z, 0..9, _ and . avaliable!"
+                    });
+                  } 
+                  else {
+                    this.setState({
+                      loginError: ""
+                    });
+                  }
+                }
+
+
+
+                if (this.state.passwordValue == "") {       //password value check
+                  this.setState({
+                    passwordError: "enter your password!"
+                  });
+                } else {
+                  if (passwordRegExp.test(this.state.passwordValue) == false) { 
+                    this.setState({
+                      passwordError: "only eng letters and digits!" 
+                    });
+                  } 
+                  else {
+                    this.setState({
+                      passwordError: "",
+                  });
+                }
+                
+              }
+                //this.setState({
+                // loginValue: this.props.value,
+                // loginError: this.state.loginError,
+                //passwordValue: ""
+                // });
+                console.log(this.state.loginValue);
               }}
             >
-              <input
+              <input //LOGIN
                 type="text"
-                placeholder="login" 
-                value={this.props.loginValue}
+                placeholder="login"
+                value={this.state.loginValue}
+                onChange={this.handleChangeValue.bind(this)}
               />
               <br />
-              <span className="login__error">
-                {this.state.loginError}
-              </span>
+              <span className="login__error">{this.state.loginError}</span>
               <br />
-              <input
-                type="password"
+              <input //PASSWORD
+                type="text"
                 placeholder="password"
                 value={this.state.passwordValue}
+                onChange={this.handleChangePassword.bind(this)}
               />
               <br />
-              <span className="login__error">
-                {this.state.passwordError}
-              </span>
+              <span className="login__error">{this.state.passwordError}</span>
               <br />
               <span className="additional">
                 <Link to="">forgot password?</Link>
               </span>
               <div className="login__btn">
                 {/* <Link to="/"> */}
-                  <SearchButton content="Confirm" />
+                <SearchButton content="Confirm" />
                 {/* </Link> */}
               </div>
             </form>
